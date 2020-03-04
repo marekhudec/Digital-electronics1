6b3397d429701b8568552b136e926dff73b9bca6
lab661 <lab661@vutbr.cz>
2020-03-04 12:47:40 +0100

    Binary counter

---
+++ Labs/05-counter_binary/binary_counter/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/HudecZboril/Digital-electronics-1/Labs/05-counter_binary/binary_counter/binary_counter_top_isim_beh.exe" -prj "/home/lab661/Documents/HudecZboril/Digital-electronics-1/Labs/05-counter_binary/binary_counter/binary_counter_top_beh.prj" "work.binary_counter_top" 
