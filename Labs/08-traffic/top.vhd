941d8ea7952af8d138bee847d28f23927e6ece5f
marekhudec <60688335+marekhudec@users.noreply.github.com>
2020-03-28 11:14:47 +0100

    Add files via upload

---
+++ Labs/08-traffic/top.vhd
@@ -0,0 +1,37 @@
+------------------------------------------------------------------------
+--
+-- Implementation of 4-bit binary counter.
+-- Xilinx XC2C256-TQ144 CPLD, ISE Design Suite 14.7
+--
+-- Copyright (c) 2019-2020 Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity top is
+
+port(
+	clk_in: in STD_LOGIC;
+	BTN0: in STD_LOGIC;
+	CPLD_leds_o: out STD_LOGIC_VECTOR(6-1 downto 0)
+	);
+
+end top;
+
+architecture Behavioral of top is
+
+begin
+	traffic: entity work.traffic
+	port map (
+		clk_i => clk_in,         
+		srst_n_i => BTN0,			
+		lights_o => CPLD_leds_o
+	);
+
+
+end Behavioral;
---
+++ Labs/08-traffic/traffic_light.vhd
@@ -0,0 +1,99 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity  traffic is
+port ( clk_i: in std_logic;
+		 srst_n_i: in std_logic;
+		 lights_o: out std_logic_vector(6-1 downto 0)
+		 );
+
+end traffic;
+architecture traffic of traffic is
+    type state_type is (green_red, yellow_red, red_red, red_green,red_yellow,red_red_2);
+	 signal state: state_type;
+    signal s_count: unsigned(3 downto 0);
+    constant SEC5: unsigned(3 downto 0) := "1111";
+    constant SEC1: unsigned(3 downto 0) := "0011";
+  
+begin
+process(clk_i,srst_n_i)
+begin
+    if rising_edge(clk_i) then
+	 if srst_n_i = '0' then
+	 state <= green_red;
+	 s_count <= x"0"; 
+		else 
+		case state is
+		when green_red => 
+			if s_count < SEC5 then
+				state <= green_red;
+				s_count <= s_count +1;
+			else 
+				state <= yellow_red;
+				s_count <= x"0";
+			end if;
+		when yellow_red =>
+			if s_count < SEC1 then
+				state <= yellow_red;
+				s_count <= s_count +1;
+			else 
+				state <= red_red;
+				s_count <= x"0";
+			end if;
+		when red_red => 
+			if s_count < SEC1 then
+				state <= red_red;
+				s_count <= s_count + 1;
+			else 
+				state <= red_green;
+				s_count <= x"0";
+			end if;
+		when red_green => 
+			if s_count < SEC1 then
+				state <= red_green;
+				s_count <= s_count +1;
+			else 
+				state <= red_yellow;
+				s_count <= x"0";
+			end if;
+		when red_yellow =>
+			if  s_count < SEC1 then
+				 state <= red_yellow;
+				 s_count <= s_count +1;
+			else
+				 state <= red_red_2;
+				 s_count <= x"0";
+				end if;
+		when red_red_2 =>
+			if  s_count < SEC1 then
+				 state <= red_red_2;
+				 s_count <= s_count +1;
+			else
+				 state <= green_red;
+				 s_count <= x"0";
+				end if;
+		when others => 
+				 state <= green_red;
+			end case;
+		end if;
+	end if;
+end process;
+	
+	C2: process(state)
+	begin
+
+	case state is
+			when green_red => lights_o <= "100001";
+			when yellow_red => lights_o <= "100010";
+			when red_red => lights_o<= "100100";
+			when red_green => lights_o <= "001100";
+			when red_yellow => lights_o <= "010100";
+			when red_red_2 => lights_o <= "100100";
+			when others	=> lights_o <= "100001";
+		end case;
+	
+	end process; 
+end traffic;    
+    
